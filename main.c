2358623e48d1b7f7ced4e9219e7acf55900d48e6
Sofia <sofiavillelav@gmail.com>
2026-08-20 15:40:08 -0300

    feat: loop principal e ler terminal

--- main.c
+++ main.c
@@ -15,19 +15,46 @@ typedef struct Cadastro{
 
 int main(int argc, char **argv){
     char task[MAX_ARG]; 
-    if(argc > 2){
-        printf("Erro: numero incorreto de argumentos.")
-        exit(1);
-    }
+    int should_run = 1;
+    while(should_run){
 
-    if(argv[1] == NULL){ 
-        printf("processflow> ");
-        fflush(stdout);
-        fgets(task, MAX_ARG, stdin);
-    }
+        if(argc > 2){
+            printf("Erro: numero incorreto de argumentos.");
+            exit(1);
+        }
+    
+        if(argv[1] == NULL){ 
+            printf("processflow> ");
+            fflush(stdout);
+            if(fgets(task, MAX_ARG, stdin) == NULL) break; 
+            task[strlen(task) - 1] = '\0';
+            if(strcmp(task, "exit") == 0) should_run = 0;
+                
+        }
+    
+        else { 
+            FILE *workfile = fopen(argv[1], "r");
+            if(workfile == NULL){
+                printf("Erro: nao foi possivel abrir o arquivo. ");
+                exit(1);
+            }
+            
+            while(1){
+                if(fgets(task, MAX_ARG, workfile) == NULL) {
+                    should_run = 0;
+                    break; 
 
-    else { 
-        FILE *workfile = fopen(argv[1], "r");
+                }
+                task[strlen(task) - 1] = '\0';
+                printf("%s\n", task);
+                if(strcmp(task, "exit") == 0 ){
+                    should_run = 0;
+                    break;
+                
+                } 
+            }
+        }
+        printf("teste de rodagem, should run deve estar zerado: %d ", should_run);
     }
 
 }
